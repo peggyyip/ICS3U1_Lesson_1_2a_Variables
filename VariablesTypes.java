4c89bf8e9d1c0a0d46ef6443f16f4a3660c3ef9d
peggyyip <peggy.yip25@ycdsbk12.ca>
2024-02-09 14:15:46 -0500

    Updated VariablesTypes.java

--- VariablesTypes.java
+++ VariablesTypes.java
@@ -8,9 +8,13 @@ class VariablesTypes extends ConsoleProgram {
     int fouls = 0;
 
     //define a string variable
-    // Assign a value to firstName
-   //Print out the value of the String variable
+    String firstName; 
 
+    // Assign a value to firstName
+    firstName = "Peggy";
+
+   //Print out the value of the String variable
+    System.out.println(firstName);
     
   }
 }
